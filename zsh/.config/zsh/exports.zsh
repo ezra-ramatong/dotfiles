4df51d9a24a5ce97ee054191d301f64c9f79d834
Ezra Ramatong <128799204+ezra-ramatong@users.noreply.github.com>
2023-09-09 11:51:50 +0200

    chore(exports): move history variables to exports

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -5,6 +5,11 @@ export VISUAL="$EDITOR"
 export TERMINAL="alacritty"
 export BROWSER="firefox"
 
+# History
+export HISTFILE=~/.cache/zsh/history
+export HISTSIZE=10000
+export SAVEHIST=10000
+
 # Paths
 export PATH="$HOME/.local/bin":$PATH
 export PATH=$HOME/.config/rofi/scripts:$PATH # Rofi Applets, Launchers and Powermenu scrips
