2c831af9aac6d7e14ce15676507865bdf634e5b0
Ezra Ramatong <128799204+ezra-ramatong@users.noreply.github.com>
2023-09-11 23:00:59 +0200

    add GOBIN directory to PATH

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -14,3 +14,4 @@ export SAVEHIST=10000
 # Paths
 export PATH="$HOME/.local/bin":$PATH
 export PATH=$HOME/.config/rofi/scripts:$PATH # Rofi Applets, Launchers and Powermenu scrips
+export PATH="$PATH:$(go env GOBIN):$(go env GOPATH)/bin"
